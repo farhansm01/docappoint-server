3885a2e8ca864a9593c2ebed910faea765e2ebff
Farhan Sadiq <farhansadiq2021@gmail.com>
2026-05-22 14:48:35 +0600

    added all the get route to get the information of doctors

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb')
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -24,13 +23,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect()
-    // ping to confirm connection
-    await client.db("admin").command({ ping: 1 })
-    console.log("Pinged! Successfully connected to MongoDB!")
 
-    app.get('/', (req, res) => {
-      res.send('DocAppoint server is running')
+    const db = client.db("docappoint")
+    const doctorsCollection = db.collection("doctors")
+
+
+     // get all doctors
+    app.get('/doctors', async (req, res) => {
+      const result = await doctorsCollection.find().toArray()
+      res.send(result)
     })
+
+    // get top 3 rated doctors
+    app.get('/doctors/top', async (req, res) => {
+      const result = await doctorsCollection
+        .find()
+        .sort({ rating: -1 })
+        .limit(3)
+        .toArray()
+      res.send(result)
+    })
+
+    // get single doctor
+    app.get('/doctors/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await doctorsCollection.findOne(query)
+      res.send(result)
+    })
+
 
   } finally {
     // keep open
@@ -38,3 +59,7 @@ async function run() {
 }
 
 run().catch(console.dir)
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
