17f56ec9a504fe0c50b9bf3646be258de2f6b18f
Farhan Sadiq <farhansadiq2021@gmail.com>
2026-05-22 18:25:51 +0600

    added appointments route for POST method

--- index.js
+++ index.js
@@ -1,56 +1,63 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // mongodb
-const uri = process.env.DB_URI
+const uri = process.env.DB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
-})
+  },
+});
 
 async function run() {
   try {
-    await client.connect()
+    await client.connect();
 
-    const db = client.db("docappoint")
-    const doctorsCollection = db.collection("doctors")
+    const db = client.db("docappoint");
+    const doctorsCollection = db.collection("doctors");
+    const appointmentsCollection = db.collection("appointments")
 
-
-     // get all doctors
-    app.get('/doctors', async (req, res) => {
-      const result = await doctorsCollection.find().toArray()
-      res.send(result)
-    })
+    // get all doctors
+    app.get("/doctors", async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.send(result);
+    });
 
     // get top 3 rated doctors
-    app.get('/doctors/top', async (req, res) => {
+    app.get("/doctors/top", async (req, res) => {
       const result = await doctorsCollection
         .find()
         .sort({ rating: -1 })
         .limit(3)
-        .toArray()
-      res.send(result)
-    })
+        .toArray();
+      res.send(result);
+    });
 
     // get single doctor
-    app.get('/doctors/:id', async (req, res) => {
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) }
-      const result = await doctorsCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // book appointment
+    app.post("/appointments", async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      res.send(result);
+    });
 
 
   } finally {
@@ -58,8 +65,8 @@ async function run() {
   }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
